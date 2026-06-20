245f0b6ff7fda33348a6a8ac1206f23cbb37c200
klouzd <227563190+klouzd@users.noreply.github.com>
2026-06-20 05:30:19 +0500

    changed colorscheme to gruvbox dark hard

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,25 +1,24 @@
 return {
-    {
-        "blazkowolf/gruber-darker.nvim",
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.cmd.colorscheme "gruber-darker"
-            vim.api.nvim_set_hl(0, "@lsp.type.function", { link = "@function" })
-            vim.api.nvim_set_hl(0, "@lsp.type.method", { link = "@function" })
-            vim.api.nvim_set_hl(0, "@lsp.type.variable", { link = "@variable" })
-            vim.api.nvim_set_hl(0, "@lsp.type.parameter", { link = "@variable" })
-            vim.api.nvim_set_hl(0, "@lsp.type.type", { link = "@type" })
-            vim.api.nvim_set_hl(0, "@lsp.type.keyword", { link = "@keyword" })
-        end
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-        opts = {
-            theme = 'gruber-darker',
-        }
-    },
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("gruvbox").setup({
+        contrast = "hard",
+      })
+      vim.cmd.colorscheme("gruvbox")
+      vim.api.nvim_set_hl(0, "@lsp.type.function", { link = "@function" })
+      vim.api.nvim_set_hl(0, "@lsp.type.method", { link = "@function" })
+      vim.api.nvim_set_hl(0, "@lsp.type.variable", { link = "@variable" })
+      vim.api.nvim_set_hl(0, "@lsp.type.parameter", { link = "@variable" })
+      vim.api.nvim_set_hl(0, "@lsp.type.type", { link = "@type" })
+      vim.api.nvim_set_hl(0, "@lsp.type.keyword", { link = "@keyword" })
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = { theme = "gruvbox" },
+  },
 }
